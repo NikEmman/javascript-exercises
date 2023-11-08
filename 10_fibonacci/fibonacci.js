fe66061905f967d645261509dd735037c6abc68d
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-11-08 03:02:39 +0200

    #10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0) return "OOPS";
+    if(num===0)return 0;
+    let num1=0;
+    let num2=1;
+    for(let i=2;i<=num;i++){
+        let current = num1+num2;
+        num1=num2;
+        num2=current;
+    }
+    return num2;
 };
 
 // Do not edit below this line
